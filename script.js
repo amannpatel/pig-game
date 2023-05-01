c7932905a61859318c4501b069ccaf59a5ec8181
Aman Patel☕ <aman17626@gmail.com>
2023-05-01 20:40:36 +0530

    changing active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements.
+const player0Ele = document.querySelector('.player--0');
+const player1Ele = document.querySelector('.player--1');
 const score0Ele = document.getElementById('score--0');
 const score1Ele = document.querySelector('#score--1');
 const current0Ele = document.getElementById('current--0');
@@ -35,6 +37,10 @@ btnRoll.addEventListener('click', function () {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         //switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+        player0Ele.classList.toggle('player--active');
+        player1Ele.classList.toggle('player--active');
     }
 })
